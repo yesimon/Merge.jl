96d1cff1ce49e1fc3f66672fa52523a0e40d3791
Simon Ye <sye737@gmail.com>
2016-04-20 17:36:38 -0400

    Merge sorted iterators.

--- src/Merge.jl
+++ src/Merge.jl
@@ -1,5 +1,59 @@
 module Merge
+import Base: start, next, done, eltype, length
+using DataStructures
+export merge_sorted
 
-# package code goes here
+immutable SortedMerger{I}
+    keyfunc::Function
+    its::Vector{I}
+end
+
+function merge_sorted(its, f=identity)
+    SortedMerger(f, its)
+end
+
+function start(it::SortedMerger)
+    el_type = eltype(eltype(it.its))
+    if el_type <: AbstractString
+        el_type = AbstractString
+    end
+    h = binary_minheap(Tuple{el_type, Int})
+    l = length(it.its)
+    its = Vector(l)
+    values = Vector{eltype(eltype(it.its))}(l)
+    for (i, xs) in enumerate(it.its)
+        s = start(xs)
+        if done(xs, s)
+            continue
+        end
+
+        (x, s) = next(xs, s)
+        key = it.keyfunc(x)
+        values[i] = x
+        its[i] = (xs, s)
+        push!(h, (key, i))
+    end
+    (h, its, values)
+end
+
+function next(it::SortedMerger, state)
+    (h, its, values) = state
+    (key, i) = pop!(h)
+    next_x = values[i]
+    (xs, s) = its[i]
+    if !done(xs, s)
+        (x, s) = next(xs, s)
+        key = it.keyfunc(x)
+        its[i] = (xs, s)
+        values[i] = x
+        push!(h, (key, i))
+    end
+    (next_x, (h, its, values))
+end
+
+function done(it::SortedMerger, state)
+    (h, its, values) = state
+    isempty(h)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,34 @@
 using Merge
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+a = [1, 2, 5, 7, 8, 11]
+b = [1, 4, 5, 6, 9, 10, 12, 16]
+expected = [1, 1, 2, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 16]
+@test collect(merge_sorted([a; b])) == expected
+@test collect(merge_sorted([a; b], x -> x * -1)) == reverse(expected)
+
+a = ["abcd", "acbd", "bcad", "db", "dbac", "dcab"]
+b = ["abcde", "acbdf", "baad", "cabd", "dba", "dc"]
+c = ["bbcde", "cfbdf", "daad", "dabd", "dba", "ec"]
+expected = sort([a; b; c])
+@test collect(merge_sorted(Vector{ASCIIString}[a, b, c])) == expected
+
+a = [1, 2, 7]
+b = []
+@test collect(merge_sorted(Array[a, b])) == [1, 2, 7]
+
+a = []
+b = [2, 5, 8]
+@test collect(merge_sorted(Array[a, b])) == [2, 5, 8]
+
+a = []
+b = []
+@test collect(merge_sorted(Array[a, b])) == []
+
+a = [1, 2, 7]
+b = []
+c = []
+d = [2, 5, 8]
+e = [100]
+f = []
+@test collect(merge_sorted(Array[a, b, c, d, e, f])) == [1, 2, 2, 5, 7, 8, 100]
